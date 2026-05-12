de9f08b648aedc20aea3921ef1b6fe1fd1440612
Suraj Yadav <42316893+surajy93@users.noreply.github.com>
2026-05-12 17:43:11 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def merge(nums, l, m, r):
+            left, right = nums[l : m + 1], nums[m + 1 : r + 1]
+            i, j, k = l, 0, 0
+            # Merge back into nums
+            while j < len(left) and k < len(right):
+                if left[j] <= right[k]:
+                    nums[i] = left[j]
+                    j += 1
+                else:
+                    nums[i] = right[k]
+                    k += 1
+                i += 1
+
+            while j < len(left):
+                nums[i] = left[j]
+                j += 1
+                i += 1
+
+            while k < len(right):
+                nums[i] = right[k]
+                k += 1
+                i += 1
+
+        def mergeSort(arr, l, r):
+            if l >= r:
+                return arr
+            m = (l + r) // 2
+            mergeSort(arr, l, m)
+            mergeSort(arr, m + 1, r)
+            merge(arr, l, m, r)
+            return arr
+
+        mergeSort(nums, 0, len(nums) - 1)
+        return nums
